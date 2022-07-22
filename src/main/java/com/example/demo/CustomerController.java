b714338812c80a0ba27a327143ad088292f24301
wim <wim.bervoets@gmail.com>
2022-07-22 15:11:16 +0300

    upgrade java 18

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,21 +1,24 @@
 package com.example.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerController.class);
 
     private final CustomerRepository customerRepository;
 
-    public CustomerController(final CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
     @GetMapping
     public Customers getCustomers() {
+        LOG.info("getCustomers");
 //        return new Customers().customers(customerRepository.findAll());
         return new Customers().customers(customerRepository.findTop3ByLastName("Bauer"));
     }
